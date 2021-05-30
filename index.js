81d5925d56164762dfc11b249dd7bce15ba71c3b
forestus <guilhermepm164@gmail.com>
2021-05-30 20:40:05 -0300

    chore: testing pm2 restart on build

--- index.js
+++ index.js
@@ -3,20 +3,11 @@ const PORT = 3000
 const app = express()
 app.use(express.json())
 app.get('/',(req,res)=>{
-res.json({message:'hello word!'})
-})
-app.get('/hellow',(req,res)=>{
     res.json({
         message:'hello word!',
         Jenkins:'hello im Jenkins!'
     })
-    })
-    app.get('/testing2',(req,res)=>{
-        res.json({
-            message:'hello word!',
-            Jenkins:'hello im Jenkins!'
-        })
-        })
+})
 app.listen(PORT,()=>{
     console.log(`Server running!`)
 })
